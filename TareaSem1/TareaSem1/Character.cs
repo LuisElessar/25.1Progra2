d07efa880bd692ee65ba6bf8ee38d792fba339f5
LuisElessar <SteveLuis4555@gmail.com>
2025-04-03 09:52:00 -0500

    Creación de la Tarea y clase Abstracta

---
+++ TareaSem1/TareaSem1/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaSem1
+{
+    internal abstract class Character
+    {
+        protected string name;
+        public string Name { get { return name; } }
+
+        public Character(string name)
+        { 
+            this.name = name; 
+        }
+    }
+}
